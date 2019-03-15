405ec6ed345ea6ecfb2b7081cb5fa52ed922b25f
Joshua Jancula <josh.jancula@sebrio.com>
2019-03-15 00:45:14 -0400

    deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 'extended': 'false' }));
 app.use('/', express.static(path.join(__dirname, '/client/dist/client')));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 
 app.all('*', (req, res, next) => {
